fdeb6a786d1c0f5952f3d672bbe8f3c1df5b02e7
bersling <bersling@gmail.com>
2017-05-20 00:56:46 +0200

    working deploy script

---
+++ deploy.sh
@@ -0,0 +1,27 @@
+server=ubuntu@52.59.71.133
+
+# remove the old zip file if present, locally and on server
+rm -f tsmean.zip
+ssh ${server} "rm -f tsmean.zip"
+
+# zip the required directories
+zip -r tsmean.zip . -x \
+./node_modules/**\* \
+./dist/**\* >/dev/null
+
+echo ""
+echo "---------------------------------"
+echo "Upload Zip"
+echo "---------------------------------"
+
+scp tsmean.zip "${server}:~"
+
+ssh ${server} "rm -rf tsmeandir"
+ssh ${server} "unzip tsmean.zip -d tsmeandir"
+ssh ${server} "cd tsmeandir && npm install && npm run build && npm run stopforever && npm run forever"
+
+echo ""
+echo "---------------------------------"
+echo "Cleanup"
+echo "---------------------------------"
+rm tsmean.zip
