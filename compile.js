d59dcd26e561378e8cd5e1ce9997b35af66cdf73
bersling <bersling@gmail.com>
2017-06-08 14:00:55 +0200

    wip

--- compile.js
+++ compile.js
@@ -11,6 +11,26 @@ var sections = {
     'app-properties.model.ts.html': '/src/config/app-properties.model.ts',
     'local.properties.json.html': '/properties/local.properties.json',
     'test.properties.json.html': '/properties/test.properties.json'
+  },
+  'authentication': {
+    'login-router.test.ts.html': '/src/router/endpoints/login-router.test.ts',
+    'login-router.ts.html': '/src/router/endpoints/login-router.ts',
+    'passport.test.ts.html': '/src/auth/passport.test.ts',
+    'passport.ts.html': '/src/auth/passport.ts',
+    'password-cryptographer.test.ts.html': '/src/auth/password-cryptographer.test.ts',
+    'password-cryptographer.ts.html': '/src/auth/password-cryptographer.ts',
+    'user.model.ts.html': '/src/db/user.model.ts',
+    'user-dao.test.ts.html': '/src/db/user-dao.test.ts',
+    'user-dao.ts.html': '/src/db/user-dao.ts'
+  },
+  'database': {
+    'before-eachs.ts.html' : '/src/test/before-eachs.ts',
+    'dao.test.ts.html' : '/src/db/dao.test.ts',
+    'dao.ts.html' : '/src/db/dao.ts',
+    'database.test.ts.html' : '/src/db/database.test.ts',
+    'database.ts.html' : '/src/db/database.ts',
+    'database-response.model.ts.html' : '/src/db/database-response.model.ts',
+    'utils.ts.html' : '/src/utils/utils.ts'
   }
 };
 
