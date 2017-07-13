1940b3b37cf3c409832eb6af33a46d9ef7684e0f
bersling <bersling@gmail.com>
2017-07-13 08:38:46 +0200

    fix deploy script;

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 server=ubuntu@35.158.213.131
 
-npm run build
-
 if [ "${1}" == "dev" ]; then
+  npm run build
   rsync -avz --delete -e 'ssh' "dist/" "${server}:tsmeanampdev/dist"
 elif [ "${1}" == "prod" ]; then
+  npm run build
   rsync -avz --delete -e 'ssh' "dist/" "${server}:tsmeanamp/dist"
 else
  echo "Expected dev or prod as arguments"
