e9558a467866618dcf7fc4e31728ac7c5522ea85
bersling <bersling@gmail.com>
2017-07-31 20:04:58 +0200

    move to https

--- server.ts
+++ server.ts
@@ -1,7 +1,7 @@
 import * as express from 'express';
 import * as cors from 'cors';
 const server = express();
-server.use(cors({origin: ["http://localhost:8082", "http://www.tsmean.com"], credentials: true}));
+server.use(cors({origin: ["http://localhost:8082", "https://www.tsmean.com"], credentials: true}));
 server.use('/', express.static(__dirname + '/dist', {extensions: ['html']}));
 
 const port = process.argv[2] || 8082;
