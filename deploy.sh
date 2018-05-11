12debe4771b022376e16bcbcb93c04091296e6d4
bersling <bersling@gmail.com>
2018-05-11 23:40:52 +0200

    introduce auto invalidation

--- deploy.sh
+++ deploy.sh
@@ -13,8 +13,10 @@ npm run build
 
 if [ "${1}" == "dev" ]; then
   aws s3 cp dist s3://aws-website-tsmean-dev-ghyw3/ --recursive # --cache-control max-age=86400,public
+  aws cloudfront create-invalidation --distribution-id E1L4LCQDU1DD5Q --paths "/*"
 elif [ "${1}" == "prod" ]; then
   aws s3 cp dist s3://aws-website-tsmean-prod-gclcd/ --recursive # --cache-control max-age=86400,public
+  aws cloudfront create-invalidation --distribution-id E17836BQ15BE93 --paths "/*"
 fi
 
 echo "Done!"
